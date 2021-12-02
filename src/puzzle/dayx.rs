3d77da99211711bc8161aa9a8d30893a16b57844
Trond H Emaus <trond.hubertz.emaus@remarkable.no>
2021-12-02 11:04:17 +0100

    add puzzle template

---
+++ src/puzzle/dayx.rs
@@ -0,0 +1,17 @@
+pub fn solve() {
+    println!("Day x");
+
+    println!("silver - ");
+
+    println!("gold - ");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn foo() {
+        assert_eq!(0, 0);
+    }
+}
